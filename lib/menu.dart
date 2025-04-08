51864f9b4c751e56d41bcafa0cd69cdc453490ec
ViniShuet <vinishuet@gmail.com>
2025-04-08 20:47:47 -0300

    Pizza fluter

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:pizzamenu/menu_item.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: MenuItem(),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Pizza Menu"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              MenuItem(),
+              MenuItem(),
+              MenuItem(),
+              MenuItem(),
+              MenuItem(),
+              MenuItem(),
+            ]
+             ),
+        ),
     );
   }
 }
